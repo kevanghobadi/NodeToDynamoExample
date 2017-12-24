ffbe70d4d3e175042425c576dcc4c06ea2a5bef7
Kevan John Ghobadi <kevanjohnghobadi@Kevans-MacBook-Pro.local>
2017-12-24 12:20:06 -0500

    added the ability to extract a name from a form

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var csvApi = require('../controller/csvApi')
+var bodyParser = require('body-parser');
+var csvApi = require('../controller/nameAPI.js')
+
+router.use(bodyParser.urlencoded({ extended: true }));
 
 /* GET home page. */
 router.get('/test', function(req, res, next) {
   csvApi.hello(req, res);
+});
+
+router.post('/name_url', function(req, res) {
+  res.send('You sent the name: "' + req.body.param_name + '".');
 });
 
 module.exports = router;
